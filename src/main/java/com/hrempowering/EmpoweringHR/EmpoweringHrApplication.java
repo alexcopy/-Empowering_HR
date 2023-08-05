c7c10f05d93d64fc44731d43c711dddc8f42d9bc
Alex Redko <a.redko@nr-online.com>
2023-08-05 16:52:48 +0100

    initial commit

---
+++ src/main/java/com/hrempowering/EmpoweringHR/EmpoweringHrApplication.java
@@ -0,0 +1,13 @@
+package com.hrempowering.EmpoweringHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpoweringHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpoweringHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrempowering/EmpoweringHR/EmpoweringHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrempowering.EmpoweringHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpoweringHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
